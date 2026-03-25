40bfde38a139a2035abc97616390473933eecb9b
rfitushiyt <ritabekteshi12@gmail.com>
2026-03-25 16:17:32 +0100

    js 1

---
+++ lesson26/main.js
@@ -0,0 +1,21 @@
+console.log("Goodbye")
+
+var x = 5;
+
+var a = 50 + 50;
+console.log(a)
+
+var b = 20;
+var c = 30;
+var d = b + c;
+console.log(d)
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g)
+
+var h = 10;
+var i = 4;
+var j = h % i;
+console.log(j)
