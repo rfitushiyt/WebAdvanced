94dbd801fa3037739a043e1f4b7e7e3d3b4bf6b1
rfitushiyt <ritabekteshi12@gmail.com>
2026-03-30 16:14:22 +0200

    liudwahgvhyuh

--- lesson26/main.js
+++ lesson26/main.js
@@ -19,3 +19,66 @@ var h = 10;
 var i = 4;
 var j = h % i;
 console.log(j)
+
+var k = 99;
+k++;
+console.log(k)
+
+var l = 113;
+l--;
+console.log(l)
+
+var m = 20;
+var n = m**2;
+console.log(n)
+
+var o = 15;
+o+= 5;
+console.log(o)
+
+var p = 15;
+p-= 5;
+console.log(p)
+
+var q = 5;
+q*= 5;
+console.log(q)
+
+var r = 25;
+r/= 5;
+console.log(r)
+
+var s = 10;
+var t = 10;
+console.log(s == t)
+
+var u = 10;
+var v = 6;
+console.log(u != v)
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2)
+
+var num3 = 5;
+var num4 = 1;
+console.log(num3 < num4)
+
+var num5 = 123;
+var num6 = "123";
+console.log(num5 === num6)
+
+var fullName = "Digital";
+var lastName = "School";
+var fullName = fullName + " " + lastName;
+console.log(fullName)
+
+var school = "Digital School"
+console.log(`Hello from ${school}`);
+
+var age = 25;
+console.log( age >= 15 && age <= 65)
+
+var var1 = 10;
+var var2 = 20;
+console.log(var1 === 10 || var2 === 10)
---
+++ lesson27/main.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("This message is inside the function");
+}
+
+showMessage(); 
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(sum(5, 10));
