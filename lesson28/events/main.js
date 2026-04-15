31cbbeafd75a2cf93a2568e911c73c5cf21a5539
rfitushiyt <ritabekteshi12@gmail.com>
2026-04-15 16:04:18 +0200

    lesson28 sotkamefiturealmadridnkrahasimmeliverpool

---
+++ lesson28/events/main.js
@@ -0,0 +1,24 @@
+var button1 = document.getElementById('btn1')
+var button2 = document.getElementById('btn2')
+var button3 = document.getElementById('btn3')
+var button4 = document.getElementById('btn4')
+
+button1.onclick = function()
+{
+    alert('hello');
+}
+
+button2.onmouseover = function()
+{
+    alert('hello');
+}
+
+button3.onmouseleave = function()
+{
+    alert('hello');
+}
+
+button4.onmousewheel = function()
+{
+    alert('hello');
+}
