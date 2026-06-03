121026e7a3ab9025c58faf788db039084cd1b027
rfitushiyt <ritabekteshi12@gmail.com>
2026-06-03 16:12:38 +0200

    khbiyvwvf

---
+++ lesson36/part1/main.js
@@ -0,0 +1,15 @@
+$(window).ready(function(){
+    alert('It is working guys!!!!!')
+});
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
---
+++ lesson36/part2/main.js
@@ -0,0 +1,27 @@
+$('li').click(function(){
+    alert('clicked')
+});
+
+$('#btn').click(function(){
+    console.log($('#h1').text());
+    $('#h1').text('Testing text');
+    $('#h1').append('Extra text');
+});
+
+$('#btn2').on('click', function(){
+    console.log('Second button is clicked')
+});
+
+$('#myDiv').click(function(){
+    $(this).addClass('clicked');
+})
+.find('span')
+.attr('title', 'Hover over me');
+
+$('#btn3').click(function(){
+    $('.hidden').show('slow');
+});
+
+$('#btn4').click(function(){
+    $('.hidden').hide();
+});
