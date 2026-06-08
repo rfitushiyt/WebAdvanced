e3c8fb6a702164bd39db433ba521bf1306c672ae
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-06-08 16:11:55 +0200

    commit

--- lesson36/part2/main.js
+++ lesson36/part2/main.js
@@ -25,3 +25,26 @@ $('#btn3').click(function(){
 $('#btn4').click(function(){
     $('.hidden').hide();
 });
+
+$('#square').click(function(){
+
+    $('#square').animate({
+        'width':'200px',
+        'height':'200px'
+    });
+});
+
+$('.testing').click(function(){
+
+    $('.testing').animate({
+        'left': '+=50',
+        'opacity': 0.25,
+        'fontSize': '30px'
+    },
+    300,
+    function(){
+        console.log("Animation is complete")
+    }
+);
+});
+
